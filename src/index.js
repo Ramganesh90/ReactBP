821bbe37e455d38a05622031d601f682b889f48d
Ramganesh <ramg9048@gmail.com>
2019-11-23 22:41:20 +0530

    Added eslint and babel

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react"
 import { render } from 'react-dom';
 
+// eslint-disable-next-line react/prop-types
+const Greetings = (props) => <h1><i>{props.message}</i></h1>;
+
+
 function Hi() {
-    return <h1><i>Buddy BoilerPlate is working in React</i></h1>
+    return <Greetings message="Buddy BoilerPlate is working in React" />;
 }
 
 
